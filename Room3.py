127f31cf8d82e7beed5a09cd4c2f3ac0a74a16da
Daniel Du <dadu@ctemc.org>
2018-02-05 14:17:06 -0500

    started room 3

---
+++ Room3.py
@@ -0,0 +1,20 @@
+print "=================================================================================="
+print "You have entered the engine room."
+print "In order to redirect the ship away from the incoming asteroid, you must diagnose"
+print "the issue with the engine and access the computer that houses the error."
+print "=================================================================================="
+
+passwords = "ketchup, mustard, ranch"
+
+print "There appears to be three routes that you can take."
+
+decision = raw_input("Would you like to go left, straight, or right?")
+
+if decision == "left":
+    pass
+elif decision == "straight":
+    pass
+elif decision == "right":
+    pass
+else:
+    pass
