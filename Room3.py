604699feb46772a04ff8c3b35c8b456d8a01c1e6
Daniel Du <dadu@ctemc.org>
2018-02-09 14:14:02 -0500

    more room3

--- Room3.py
+++ Room3.py
@@ -5,16 +5,77 @@ print "the issue with the engine and access the computer that houses the error."
 print "=================================================================================="
 
 passwords = "ketchup, mustard, ranch"
+backpack = []
 
 print "There appears to be three routes that you can take."
 
-decision = raw_input("Would you like to go left, straight, or right?")
-
-if decision == "left":
-    pass
-elif decision == "straight":
-    pass
-elif decision == "right":
-    pass
-else:
-    pass
+def initDecision():
+    decision = raw_input("Would you like to go left, straight, or right?")
+    if decision == "left":
+        initLeft();
+    elif decision == "straight":
+        initStraight();
+    elif decision == "right":
+        initRight();
+    else:
+        print "That is not a valid option"
+    
+def initLeft():
+    print "There seems to be a computer and the ship's engine in front of you."
+    decision = raw_input("Would you check the computer and engine out?")
+    if decision == "yes":
+        decision2 = raw_input("Which one would you like to access first?")
+        if decision2 == "computer":
+            print "The computer screen seems to require a password"
+            while password == false:
+                decision3 = raw_input("Enter a password")
+                if decision3 == "ketchup":
+                    password = true
+                else:
+                    print "Sorry, that is incorrect"
+        elif decision2 == "engine":
+            
+        else: 
+            initLeft()
+    else:
+        "You return to the beginning intersection"
+    
+def initStraight():
+    print "The door to the next room lies in front of you."
+    print "There seems to be a loose bolt in the door."
+    if "wrench" in backpack:
+        decision = raw_input("Would you like to use your wrench to fix the loose bolt?")
+        if decision == "yes":
+            print "You successfully fix the door, which now opens to the next room."
+            print "You enter the next room."
+        else:
+            "You return to the beginning intersecton"
+    
+def initRight():
+    print "There seems to be a set of three cabinets in front of you."
+    decision = raw_input("Would you like to examine the cabinets?"
+        if decision == "yes":
+            decision2 = raw_input("Which cabinet would you like to open (1/2/3)?"
+                if decision == "1":
+                    print "There seems to be a welder in the cabinet."
+                    decision3 = raw_input("Would you like to put the welder into your backpack?"
+                    if decision3 == "yes":
+                        backpack.append("welder")
+                    else:
+                        initRight()
+                elif decision == "2":
+                    print "There seems to be a hammer in the cabinet."
+                    decision3 = raw_input("Would you like to put the hammer into your backpack?"
+                    if decision3 == "yes":
+                        backpack.append("hammer")
+                    else:
+                        initRight()
+                else:
+                    print "There seems to be a wrench in the cabinet."
+                    decision3 = raw_input("Would you like to put the wrench into your backpack?"
+                    if decision3 == "yes":
+                        backpack.append("wrench")
+                    else:
+                        initRight()
+        else:
+            "You return to the beginning intersection"
