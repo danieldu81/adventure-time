b3c4203f85dd08ce8d448365d672bf3794338de0
Sidhu Arakkal <sidhu.s.arakkal@gmail.com>
2018-02-16 13:31:13 -0500

    Introduction
    Added the introduction to the game.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print 'This game is solvable in O(n)'
+import sys, time
+
+asciiTitle = '         ___           ___         ___           ___           ___                    ___           ___           ___           ___     \n'
+asciiTitle += '        /  /\\         /  /\\       /  /\\         /  /\\         /  /\\                  /  /\\         /  /\\         /  /\\         /  /\\    \n'
+asciiTitle += '       /  /:/_       /  /::\\     /  /::\\       /  /:/        /  /:/_                /  /::\\       /  /::\\       /  /:/        /  /:/_   \n'
+asciiTitle += '      /  /:/ /\\     /  /:/\\:\\   /  /:/\\:\\     /  /:/        /  /:/ /\\              /  /:/\\:\\     /  /:/\\:\\     /  /:/        /  /:/ /\\  \n'
+asciiTitle += '     /  /:/ /::\\   /  /:/~/:/  /  /:/~/::\\   /  /:/  ___   /  /:/ /:/_            /  /:/~/:/    /  /:/~/::\\   /  /:/  ___   /  /:/ /:/_ \n'
+asciiTitle += '    /__/:/ /:/\\:\\ /__/:/ /:/  /__/:/ /:/\\:\\ /__/:/  /  /\\ /__/:/ /:/ /\\          /__/:/ /:/___ /__/:/ /:/\\:\\ /__/:/  /  /\\ /__/:/ /:/ /\\ \n'
+asciiTitle += '    \\  \\:\\/:/~/:/ \\  \\:\\/:/   \\  \\:\\/:/__\\/ \\  \\:\\ /  /:/ \\  \\:\\/:/ /:/          \\  \\:\\/:::::/ \\  \\:\\/:/__\\/ \\  \\:\\ /  /:/ \\  \\:\\/:/ /:/\n'
+asciiTitle += '     \\  \\::/ /:/   \\  \\::/     \\  \\::/       \\  \\:\\  /:/   \\  \\::/ /:/            \\  \\::/~~~~   \\  \\::/       \\  \\:\\  /:/   \\  \\::/ /:/ \n'
+asciiTitle += '      \\__\\/ /:/     \\  \\:\\      \\  \\:\\        \\  \\:\\/:/     \\  \\:\\/:/              \\  \\:\\        \\  \\:\\        \\  \\:\\/:/     \\  \\:\\/:/  \n'
+asciiTitle += '        /__/:/       \\  \\:\\      \\  \\:\\        \\  \\::/       \\  \\::/                \\  \\:\\        \\  \\:\\        \\  \\::/       \\  \\::/   \n'
+asciiTitle += '        \\__\\/         \\__\\/       \\__\\/         \\__\\/         \\__\\/                  \\__\\/         \\__\\/         \\__\\/         \\__\\/'
+
+title = 'Welcome to Space Race, an interactive text-based video game created by students for students!\n\n'
+
+print(asciiTitle)
+print('{:^140s}'.format(title))
+
+print("Get ready for a FANTASTIC adventure! Your adventure will start in...")
+'''for i in xrange(5,0,-1):
+    time.sleep(1)
+    print i'''
+    
+print("\nThe year is 20XX. Your name is Zeév from planet Penséev and currently you are hurtling 65 km/s towards an unknown planet.As\none of the few survivors of the human race, it is your responsibility to travel from planet to planet in the hopes of finding a new\nhome for your people. Unbeknownst to you, the ship has malfunctioned and as a result you have been woken up early from your cryosleep...\n\n")
+
+print('========================================================================================================================================')
